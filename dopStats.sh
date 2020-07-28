d6becb6812eb91610421d552cf41deb78f867bca
Josh Gainey <53830607+pivotal-Josh-Gainey@users.noreply.github.com>
2020-07-28 14:30:22 -0400

    Updated display because it was missing one doppler
    It includes all now.

--- dopStats.sh
+++ dopStats.sh
@@ -20,12 +20,11 @@ display(){
         if [[ $DOPINFO_DISPLAY_LOOP_FIRST_ITERATION == 'true' ]]; then
             printf "%40s %15s %15s %15s %15s\n" "INDEX" "INGRESS_RATE" "INGRESS_DROP" "EGRESS_RATE" "EGRESS_DROP"
             DOPINFO_DISPLAY_LOOP_FIRST_ITERATION='false'
-        else
-            INGRESS=`cut -d " " -f2 <<< "$DOPINFO_INGRESS" | grep $dopindex -A 1 | grep -v $dopindex`
-            INDROP=`cut -d " " -f2 <<< "$DOPINFO_INGRESS_DROP" | grep $dopindex -A 1 | grep -v $dopindex`
-            EGRESS=`cut -d " " -f2 <<< "$DOPINFO_EGRESS" | grep $dopindex -A 1 | grep -v $dopindex`
-            EDROP=`cut -d " " -f2 <<< "$DOPINFO_EGRESS_DROP" | grep $dopindex -A 1 | grep -v $dopindex`
-            printf "%40s %15.0f %15.0f %15.0f %15.0f\n" $dopindex "$INGRESS" "$INDROP" "$EGRESS" "$EDROP" 
         fi
+        INGRESS=`cut -d " " -f2 <<< "$DOPINFO_INGRESS" | grep $dopindex -A 1 | grep -v $dopindex`
+        INDROP=`cut -d " " -f2 <<< "$DOPINFO_INGRESS_DROP" | grep $dopindex -A 1 | grep -v $dopindex`
+        EGRESS=`cut -d " " -f2 <<< "$DOPINFO_EGRESS" | grep $dopindex -A 1 | grep -v $dopindex`
+        EDROP=`cut -d " " -f2 <<< "$DOPINFO_EGRESS_DROP" | grep $dopindex -A 1 | grep -v $dopindex`
+        printf "%40s %15.0f %15.0f %15.0f %15.0f\n" $dopindex "$INGRESS" "$INDROP" "$EGRESS" "$EDROP" 
     done
 }
