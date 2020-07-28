06aaafa7008dbc679920fcc82a8d1d919f2bb7ad
Josh Gainey <53830607+pivotal-Josh-Gainey@users.noreply.github.com>
2020-07-28 14:04:42 -0400

    Update dopStats.sh

--- dopStats.sh
+++ dopStats.sh
@@ -3,9 +3,9 @@
 initVars(){
     export DOPINFO_INDEXES_UNSORTED=`cf query "ingress{source_id='doppler'}" |  jq -r '.data.result[] | .metric.index'`
     export DOPINFO_INDEXES=`echo "$DOPINFO_INDEXES_UNSORTED" | tr ' ' '\n' | sort | tr '\n' ' '`
-    export DOPINFO_INGRESS=`cf query "ingress{source_id='doppler'}"|  jq -r '.data.result[] | .metric.index,.value[1]'`
+    export DOPINFO_INGRESS=`cf query "rate(ingress{source_id='doppler'}[30s])"|  jq -r '.data.result[] | .metric.index,.value[1]'`
     export DOPINFO_INGRESS_DROP=`cf query "dropped{source_id='doppler', direction='ingress'}" |  jq -r '.data.result[] | .metric.index,.value[1]'`
-    export DOPINFO_EGRESS=`cf query "egress{source_id='doppler'}" |  jq -r '.data.result[] | .metric.index,.value[1]'`
+    export DOPINFO_EGRESS=`cf query "rate(egress{source_id='doppler'}[30s])" |  jq -r '.data.result[] | .metric.index,.value[1]'`
     export DOPINFO_EGRESS_DROP=`cf query "dropped{source_id='doppler', direction='egress'}" |  jq -r '.data.result[] | .metric.index,.value[1]'`
     export DOPINFO_DISPLAY_LOOP_FIRST_ITERATION='true'
 }
